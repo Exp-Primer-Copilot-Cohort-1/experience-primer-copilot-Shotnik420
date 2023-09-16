a4845d464cfb8c92abda0cf9e3fdfc46523690ae
Daniel Szota <62155481+Shotnik420@users.noreply.github.com>
2023-09-16 21:39:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,75 +1,54 @@
-//create a web server
-const express = require('express');
-const router = express.Router();
-//import the comments schema
-const commentsSchema = require('../schema/commentsSchema');
+// Create web server application
 
-//create a route to get all comments
-router.get('/', async (req, res) => {
-    try {
-        const comments = await commentsSchema.find();
-        res.json(comments);
-    } catch (err) {
-        res.json({ message: err });
-    }
+var express = require('express'); // Load express module
+var bodyParser = require('body-parser'); // Load body parser module
+var app = express(); // Create express app
+var http = require('http').Server(app); // Create http server
+var io = require('socket.io')(http); // Create socket.io server
+var mongoose = require('mongoose'); // Load mongoose module
+var port = process.env.PORT || 3000; // Set port number
+
+app.use(express.static(__dirname)); // Set static directory
+app.use(bodyParser.json()); // Set body parser to json
+app.use(bodyParser.urlencoded({extended: false})); // Set body parser to urlencoded
+
+// Set mongoose connection
+var dbUrl = 'mongodb://localhost:27017/comments';
+var Message = mongoose.model('Message', {
+  name: String,
+  message: String
 });
 
-//create a route to get a specific comment
-router.get('/:commentId', async (req, res) => {
-    try {
-        const comments = await commentsSchema.findById(req.params.commentId);
-        res.json(comments);
-    } catch (err) {
-        res.json({ message: err });
-    }
+// Get request
+app.get('/messages', (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
 });
 
-//create a route to submit a comment
-router.post('/', async (req, res) => {
-    const comments = new commentsSchema({
-        name: req.body.name,
-        comment: req.body.comment,
-        rating: req.body.rating,
-        date: req.body.date,
-    });
-    try {
-        const savedComment = await comments.save();
-        res.json(savedComment);
-    } catch (err) {
-        res.json({ message: err });
+// Post request
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) => {
+    if (err) {
+      sendStatus(500);
     }
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  });
 });
 
-//create a route to update a specific comment
-router.patch('/:commentId', async (req, res) => {
-    try {
-        const updatedComment = await commentsSchema.updateOne(
-            { _id: req.params.commentId },
-            {
-                $set: {
-                    name: req.body.name,
-                    comment: req.body.comment,
-                    rating: req.body.rating,
-                    date: req.body.date,
-                },
-            }
-        );
-        res.json(updatedComment);
-    } catch (err) {
-        res.json({ message: err });
-    }
+// Socket.io connection
+io.on('connection', (socket) => {
+  console.log('User connected');
 });
 
-//create a route to delete a specific comment
-router.delete('/:commentId', async (req, res) => {
-    try {
-        const deletedComment = await commentsSchema.deleteOne({
-            _id: req.params.commentId,
-        });
-        res.json(deletedComment);
-    } catch (err) {
-        res.json({ message: err });
-    }
+// Connect to mongoose
+mongoose.connect(dbUrl, {useMongoClient: true}, (err) => {
+  console.log('MongoDB connection', err);
 });
 
-module.exports = router;const express = require('express');
+// Set server to listen
+var server = http.listen(port, () => {
+  console.log('Server listening on port', server.address().port);
+});
